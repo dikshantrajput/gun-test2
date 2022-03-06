e8328798631af5237705bcc98800b341d616d253
Dikshant Rajput <dikshantraj2001@gamil.com>
2022-03-06 23:25:11 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require("express");
-const Gun = require("gun");
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const Gun = require('gun')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const port = 1000;
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
+app.use(cors());
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
 app.use(Gun.serve);
 
-const server = app.listen(port, () => {
-  console.log("Listening at: http://localhost://" + port);
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
+
+const PORT = process.env.PORT || 5001;
+
+const server = app.listen(3001, () =>
+  console.log('Express server is running on ' + 3001)
+);
 
 Gun({ web: server });
